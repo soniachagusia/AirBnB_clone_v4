7d8b166ce9a870b3e802616afb03b7cc64d89fbe
Sonia Chagusia <navalayochagusia@gmail.com>
2024-05-17 09:34:17 +0000

    end

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" A Flask web application
+listening  on 0.0.0.0, port 5000"""
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """Displays a text"""
+    return("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays a text 'HBNB' """
+    return("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_fun(text):
+    """Displays c followed by a text variable value """
+    return "C" + text.replace("_", " ")
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """Displays c followed by a text variable value """
+    return "Python{}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Displays "n is a number" only if n is an int"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
